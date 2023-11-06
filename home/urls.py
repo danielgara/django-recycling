75e6005caafc1740808e831814f504ea4b7a31f2
Daniel Correa <yo@danielgara.com>
2023-11-06 17:12:57 -0500

    imp: flake8 linter

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [    
+urlpatterns = [
     path('', views.index, name='home.index'),
     path('about', views.about, name='home.about'),
 ]
